3c74204bd749061a9f9d78fc6a3e4d38546fc01b
Azizbek Axtamov <azizaxtamov0201@gmail.com>
2026-02-08 14:20:57 +0500

    everything is updated

--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -1,11 +1,14 @@
-using Microsoft.EntityFrameworkCore;
 using PokemonReviewApp.Data;
 using PokemonReviewApp;
+using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddTransient<Seed>();
@@ -20,20 +23,21 @@ builder.Services.AddDbContext<DataContext>(options =>
     )
 );
 
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwagger();
+     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
 
+
+
 app.Run();
+
 
