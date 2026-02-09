ec91b38b069ebe1b4adeafba8d42c3cd53627902
Azizbek Axtamov <azizaxtamov0201@gmail.com>
2026-02-09 15:00:50 +0500

    This push is for test:

---
+++ PokemonReviewApp/Controllers/OwnerController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("[controller]")]
+    public class OwnerController : Controller
+    {
+        private readonly ILogger<OwnerController> _logger;
+
+        public OwnerController(ILogger<OwnerController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
